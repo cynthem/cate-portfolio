e4457d3e8961d9586a935828f4edd42bf48b7cd2
doozles411 <cindy.phelps01@gmail.com>
2023-11-29 16:42:18 -0800

    Install Framer Motion

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Header() {
     return (
         <div className="header">
-            header
+            <div className='heading'></div>
+            <div className='images'></div>
         </div>
     );
 }
