3f78a59c967f460ef1f024789624a95d0e655a2c
doozles411 <cindy.phelps01@gmail.com>
2023-11-29 16:43:33 -0800

    Add Motion animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import { motion } from "framer-motion";
+import { WindupChildren, Pace } from "windups";
+import smile from '../assets/images/smile.png';
+import yoga from '../assets/images/yoga.png';
+import dip from '../assets/images/dip.png';
+import salsa from '../assets/images/salsa.png';
 
 export default function Header() {
+    const animateImg = {
+        rest: {
+            scale: 1,
+            transition: { duration: 0.5 }
+        },
+        hover: {
+            scale: 1.2,
+            transition: { duration: 0.5 }
+        }
+    }
+    
     return (
         <div className="header">
             <div className='heading'></div>
