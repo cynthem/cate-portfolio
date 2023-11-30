fd297c79eda36c343c389a683852126cd4f6f617
doozles411 <cindy.phelps01@gmail.com>
2023-11-29 16:28:09 -0800

    Import and render components

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
+import Lessons from './components/Lessons';
+import About from './components/About';
+import Footer from './components/Footer';
 
 export default function Home() {
     return (
         <div className="home">
-            testing 123
+            <Header />
+            <Lessons />
+            <About />
+            <Footer />
         </div>
     );
 }
