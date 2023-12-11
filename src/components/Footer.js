386f8b8346e02dc4431fd5b1e878620fa076915d
doozles411 <cindy.phelps01@gmail.com>
2023-12-11 13:43:03 -0800

    Add Footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <div className="footer">
-            footer
+            <p className='copyright'><span>©</span> 2023 CMP</p>
         </div>
     );
 }
