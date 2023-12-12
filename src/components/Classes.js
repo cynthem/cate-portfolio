c0ff3c2e48e1e4db1bf9f2df977e059d581a08e8
doozles411 <cindy.phelps01@gmail.com>
2023-12-11 17:37:35 -0800

    Add header

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Classes() {
     return (
         <div className="classes">
-            classes
+            <h3 className='classes-header'>Classes</h3>
+            
         </div>
     );
 }
