6de39d8e32532afbec145488b803275101951003
doozles411 <cindy.phelps01@gmail.com>
2023-12-11 19:34:31 -0800

    Add class info and link

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -3,8 +3,19 @@ import React from 'react';
 export default function Classes() {
     return (
         <div className="classes">
-            <h3 className='classes-header'>Classes</h3>
-            
+            <h3 className='classes-header'>Group Classes</h3>
+            <p className='classes-description'>Join me at Northwestern University for group classes at the Norris University Center! Lessons are open to the public and currently include:</p>
+            <div className='classes-bachata'>
+                <p>Intro to Bachata</p>
+                <p>January 17 - February 21</p>
+                <p>Wednesdays 6-7 pm</p>
+            </div>
+            <div className='classes-swing'>
+                <p>Intro to Swing</p>
+                <p>January 17 - February 21</p>
+                <p>Wednesdays 7:15-8:15 pm</p>
+            </div>
+            <p className='classes-link'>For more information, and to register, visit the <a href="https://www.northwestern.edu/norris/arts-recreation/mini-courses/">Northwestern website</a>.</p>
         </div>
     );
 }
