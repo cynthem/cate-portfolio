5dd12ba16e2c6ea58f71d0d79bf6c8a17cdcec8e
doozles411 <cindy.phelps01@gmail.com>
2023-12-11 19:22:26 -0800

    Add icon animation

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import insta from '../assets/images/instagram.png';
 
 export default function About() {
+    const animateIcon = {
+        rest: {
+            scale: 1,
+            transition: { duration: 0.5 }
+        },
+        hover: {
+            scale: 1.2,
+            transition: { duration: 0.5 }
+        },
+        tap: {
+            scale: 0.9
+        }
+    }
+    
     return (
         <div className="about">
             about
