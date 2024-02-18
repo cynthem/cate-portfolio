6723b512f2eeb88e4990372588639fa240d2ab00
doozles411 <cindy.phelps01@gmail.com>
2024-02-18 14:06:54 -0800

    Update copyright year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
     return (
         <div className="footer">
-            <p className='copyright'><span>©</span> 2023 CMP</p>
+            <p className='copyright'><span>©</span> 2024 CMP</p>
         </div>
     );
 }
