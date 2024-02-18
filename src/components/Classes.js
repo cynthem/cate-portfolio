63cc58a7bbb63d6a4e3a1247ab7bcaa6c5dbd0db
doozles411 <cindy.phelps01@gmail.com>
2024-02-18 13:50:09 -0800

    Update class schedule for Spring 2024

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -4,17 +4,23 @@ export default function Classes() {
     return (
         <div className="classes">
             <h3 className='classes-header'>Group Classes</h3>
-            <p className='classes-description'>Join me at Northwestern University for group classes at the Norris University Center! Lessons are open to the public and currently include:</p>
+            <p className='classes-description'>Join me at Northwestern University for group classes at the Norris University Center! Lessons are open to the public.</p>
+            <p className='classes-season'>**Spring schedule**</p>
             <div className='classes-list'>
                 <div className='classes-bachata'>
                     <p className='classes-name'>Intro to Bachata</p>
-                    <p>January 17 - February 21</p>
-                    <p>Wednesdays 6-7 pm</p>
+                    <p>April 9 - May 21</p>
+                    <p>Tuesday 6-6:50 pm</p>
+                </div>
+                <div className='classes-salsa'>
+                    <p className='classes-name'>Intro to Salsa</p>
+                    <p>April 9 - May 21</p>
+                    <p>Tuesday 7-7:50 pm</p>
                 </div>
                 <div className='classes-swing'>
                     <p className='classes-name'>Intro to Swing</p>
-                    <p>January 17 - February 21</p>
-                    <p>Wednesdays 7:15-8:15 pm</p>
+                    <p>April 9 - May 21</p>
+                    <p>Tuesday 8-8:50 pm</p>
                 </div>
             </div>
             <p className='classes-link'>For more information, and to register, visit the 
